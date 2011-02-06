33b5931cc5da23730e335c438c5b3efb93ac40a6
Karl Seguin <karl@openmymind.net>
2011-02-06 21:15:43 +0800

    added support for earned timestamp

--- badgly-dotnet/Earned.cs
+++ badgly-dotnet/Earned.cs
@@ -1,12 +1,13 @@
 namespace Badg.ly
 {
+   using System;
    using Newtonsoft.Json;
 
    public class Earned
    {
       [JsonProperty("badge_id")]
-      public string BadgeId { get; set; }
-      [JsonProperty("times")]
+      public string BadgeId { get; set; }      
       public int Times { get; set; }
+      public DateTime At { get; set; }
    }
 }
